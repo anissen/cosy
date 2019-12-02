313b0276701880f5069fff9beaa6c1319377d669
Anders Nissen <andnis@gmail.com>
2019-12-02 22:11:48 +0100

    Implement pretty-printing

--- src/lox/AstPrinter.hx
+++ src/lox/AstPrinter.hx
@@ -2,27 +2,61 @@ package lox;
 
 class AstPrinter {
 	public function new() {}
-	
-	public function print(expr:Expr) {
-		return switch expr {
-			case Literal(null): 'nil';
-			case Literal(v): Std.string(v);
-			case Unary(op, e): parenthesize(op.lexeme, [e]);
-			case Binary(left, op, right): parenthesize(op.lexeme, [left, right]);
-			case Grouping(e): parenthesize('group', [e]);
+
+	var indentAmount:Int = 0;
+	var isInClass:Bool = false;
+
+	function indent():String {
+		return [ for (i in 0...indentAmount) "  " ].join("");
+	}
+
+	public function printStmt(statement:Stmt):String {
+		return switch statement {
+			case Block(statements):
+				indentAmount++;
+				var s = [ for (stmt in statements) indent() + printStmt(stmt) ].join('\n');
+				indentAmount--;
+				'{\n$s\n${indent()}}';
+			case Class(name, superclass, methods):
+				var declaration = 'class ${name.lexeme}' + (superclass != null ? ' < ${printExpr(superclass)}' : '');
+				indentAmount++;
+				isInClass = true;
+				var body = [ for (method in methods) indent() + printStmt(method) ].join('\n');
+				isInClass = false;
+				indentAmount--;
+				'$declaration {\n$body\n${indent()}}';
+			case Expression(e): '${printExpr(e)};';
+			case Function(name, params, body):
+				var declaration = '${isInClass ? "" : "fun "}${name.lexeme}';
+				var parameters = [ for (token in params) token.lexeme ].join(',');
+				var block = printStmt(Block(body));
+				'$declaration($parameters) $block';
+			case If(cond, then, el): 'if (${printExpr(cond)}) ${printStmt(then)}' + (el != null ? ' else ${printStmt(el)}' : '');
+			case Print(e): 'print ${printExpr(e)};';
+			case Return(keyword, value): 'return' + (value != null ? ' ${printExpr(value)}' : '') + ';';
+			case While(cond, body): 'while (${printExpr(cond)}) ${printStmt(body)}';
+			case Var(name, init): 'var ${name.lexeme}' + (init != null ? ' = ${printExpr(init)}' : '') + ';';
 		}
 	}
 	
-	function parenthesize(name:String, exprs:Array<Expr>) {
-		var buf = new StringBuf();
-		
-		buf.add('(');
-		buf.add(name);
-		for(expr in exprs) {
-			buf.add(' ');
-			buf.add(print(expr));
+	public function printExpr(expr:Expr):String {
+		return switch expr {
+			case Assign(name, value): '${name.lexeme} = ${printExpr(value)}';
+			case Binary(left, op, right): '${printExpr(left)} ${op.lexeme} ${printExpr(right)}';
+			case Call(callee, paren, arguments): '${printExpr(callee)}(${[ for (arg in arguments) printExpr(arg) ].join(',')})';
+			case Get(obj, name): '${printExpr(obj)}.${name.lexeme}';
+			case Grouping(e): '(${printExpr(e)})';
+			case Literal(v): if (v == null) { 'nil'; } else if (Std.is(v, String)) { '"$v"'; } else { v; };
+			case Logical(left, op, right): '${printExpr(left)} ${op.type.match(Or) ? '||' : '&&'} ${printExpr(right)}';
+			case Set(obj, name, value): '${printExpr(obj)}.${name.lexeme} = ${printExpr(value)}';
+			case This(keyword): 'this';
+			case Super(keyword, method): 'super.${method.lexeme}';
+			case Unary(op, right): '(${op.lexeme}${printExpr(right)})';
+			case Variable(name): name.lexeme;
+			case AnonFunction(params, body):
+				var parameters = [ for (token in params) token.lexeme ].join(',');
+				var block = printStmt(Block(body));
+				'fun ($parameters) $block';
 		}
-		buf.add(')');
-		return buf.toString();
 	}
 }
