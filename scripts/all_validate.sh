43807bf74058b5c397787cf4984ce162617b1d1e
Anders Nissen <andnis@gmail.com>
2020-02-29 22:37:37 +0100

    Improve validation scripts

--- scripts/all_validate.sh
+++ scripts/all_validate.sh
@@ -3,6 +3,13 @@
 echo -ne "\033[0;34m"
 echo "> Compiling cosy..."
 haxe build.hxml
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo -ne "\033[0;31m"
+    echo "Compilation failed"
+    echo -ne "\033[0m"
+    exit $retVal
+fi
 
 echo -ne "\033[0;35m"
 echo "> Validating"
@@ -24,7 +31,7 @@ for filename in test/scripts/*.cosy; do
     # use 2>&1 to redirect stderr into stdout
     # hl bin/cosy.hl $options ../test/scripts/$filename 2>&1 | diff ../test/scripts/$filename.stdout -
     #hl bin/hl/cosy.hl $filename 2>&1 | diff $filename.stdout -
-    java -jar bin/java/cosy.jar $options $filename 2>&1 | diff $filename.stdout -
+    java -jar bin/java/cosy.jar $options $filename 2>&1 | diff --unified=0 $filename.stdout -
     retVal=$?
     echo -ne "\033[0m"
     # if [ $retVal -ne 0 ]; then
