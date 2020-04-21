ec334abe59d43e1a29cf9667d77de7c5dadfbd3f
Anders Nissen <andnis@gmail.com>
2020-04-21 22:02:46 +0200

    Add initial API documentation

---
+++ scripts/docs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+haxe \
+    -main cosy.Cosy \
+    -cp src \
+    -xml docs/docs.xml \
+    -D doc-gen
+
+haxelib run dox \
+    --input-path docs/docs.xml \
+    --output-path docs/api \
+    --include cosy \
+    --title "Cosy API Reference" \
+    --toplevel-package cosy \
+    -D website "https://github.com/anissen/cosy" \
+    -D source-path "https://github.com/anissen/cosy/tree/master/src/" \
+    -D logo "cosy-logo.svg"
+
+rm docs/docs.xml
