f98d19c3e1a1b9187acd2eaca4a258f81fb6b31c
Anders Nissen <andnis@gmail.com>
2020-05-02 23:32:19 +0200

    Add unary, use shorter function names

--- src/cosy/phases/CodeGenerator.hx
+++ src/cosy/phases/CodeGenerator.hx
@@ -11,43 +11,44 @@ class CodeGenerator {
 	}
 
 	public inline function generate(stmts: Array<Stmt>): Array<String> {
-        return generateStmts(stmts);
+        return genStmts(stmts);
 	}
 
-	function generateStmts(stmts: Array<Stmt>) {
+	function genStmts(stmts: Array<Stmt>) {
         var code = [];
         for (stmt in stmts) {
-            code = code.concat(generateStmt(stmt));
+            code = code.concat(genStmt(stmt));
         }
         return code;
 	}
 
-    function generateExprs(exprs: Array<Expr>) {
+    function genExprs(exprs: Array<Expr>) {
         var code = [];
         for (expr in exprs) {
-            code = code.concat(generateExpr(expr));
+            code = code.concat(genExpr(expr));
         }
         return code;
     }
 
-	function generateStmt(stmt: Stmt): Array<String> {
+	function genStmt(stmt: Stmt): Array<String> {
 		return switch stmt {
-            case Print(keyword, expr): generateExpr(expr).concat(['op_print']);
-            case Var(name, type, init, mut, foreign): (init != null ? generateExpr(init).concat(['set_var', name.lexeme]) : []); // TODO: Should probably be an index instead of a key for faster lookup
-            case Expression(expr): generateExpr(expr);
+            case Print(keyword, expr): genExpr(expr).concat(['op_print']);
+            case Var(name, type, init, mut, foreign): (init != null ? genExpr(init).concat(['set_var', name.lexeme]) : []); // TODO: Should probably be an index instead of a key for faster lookup
+            case Expression(expr): genExpr(expr);
 			case _: trace('Unhandled statement: $stmt'); [];
 		}
 	}
 
     // TODO: We also need line information for each bytecode
-	function generateExpr(expr: Expr): Array<String> {
+	function genExpr(expr: Expr): Array<String> {
 		return switch expr {
-            case Assign(name, op, value): generateExpr(value).concat(['set_var', name.lexeme]);
-            case Binary(left, op, right): generateExpr(left).concat(generateExpr(right)).concat([binaryOpCode(op)]);
+            case Assign(name, op, value): genExpr(value).concat(['set_var', name.lexeme]);
+            case Binary(left, op, right): genExpr(left).concat(genExpr(right)).concat([binaryOpCode(op)]);
             case Literal(v) if (Std.isOfType(v, Bool)): ['push_bool', '$v'];
             case Literal(v) if (Std.isOfType(v, Float)): ['push_num', '$v'];
             case Literal(v) if (Std.isOfType(v, String)): ['push_str', '$v'];
             case Variable(name): ['get_var', name.lexeme]; // TODO: Should probably be an index instead of a key for faster lookup
+            case Unary(op, right): if (!op.type.match(Minus)) throw 'error'; genExpr(right).concat(['op_negate']);
 			case _: trace('Unhandled expression: $expr'); [];
 		}
     }
