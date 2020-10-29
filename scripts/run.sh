f1e0af6c70d9fc3f52675f0c49e3fdf60e70a4ab
Anders Nissen <andnis@gmail.com>
2020-10-29 21:41:56 +0100

    Minor

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,5 @@
 # haxe build.hxml && java -jar bin/java/cosy.jar $@ 
 # haxe build.hxml && cp bin/js/cosy.js docs/playground/cosy.js && java -jar bin/java/cosy.jar $@ 
+
+# haxe -cp src -D no-inline --run cosy.Cosy $@
 haxe -cp src --run cosy.Cosy $@
