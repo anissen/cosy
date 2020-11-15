f4a8308bf40836c9d9de0e21d53afebbfda25251
Anders Nissen <andnis@gmail.com>
2020-11-15 20:59:27 +0100

    Improve disassembler output style

--- src/cosy/phases/Disassembler.hx
+++ src/cosy/phases/Disassembler.hx
@@ -1,37 +1,63 @@
 package cosy.phases;
 
 import cosy.phases.CodeGenerator.ByteCodeOpValue;
+enum OutputPart {
+    Instruction(s: String);
+    Arg(d: Any);
+    Hint(h: Any);
+    Error(s: String);
+}
 
 class Disassembler {
-    static public function disassemble(bytes: haxe.io.Bytes): String {
+    
+    static public function disassemble(bytecode: cosy.phases.CodeGenerator.Output, colors: Bool = false): String {
+        function color(part: OutputPart): String {
+            if (!colors) return switch part {
+                case Instruction(s): s;
+                case Arg(d): d;
+                case Hint(h): h;
+                case Error(s): s;
+            };
+            return switch part {
+                case Instruction(s): '\033[1;31m$s\033[0m';
+                case Arg(d): '\033[0;33m$d\033[0m';
+                case Hint(h): '\033[0;35m$h\033[0m';
+                case Error(s): '\033[0;31m$s\033[0m';
+            }
+        }
+
         var output = '\n';
-        var program = bytes;
+        var program = bytecode.bytecode;
         final sizeFloat = 4;
         var pos = 0;
         while (pos < program.length) {
             var ipPos = pos;
             var code = program.get(pos++);
-            var disassembly = switch code {
-                case ByteCodeOpValue.PushTrue: 'push_true';
-                case ByteCodeOpValue.PushFalse: 'push_false';
+            var parts = switch code {
+                case ByteCodeOpValue.PushTrue: [Instruction('push_true')];
+                case ByteCodeOpValue.PushFalse: [Instruction('push_false')];
                 case ByteCodeOpValue.PushNumber: 
                     var num = program.getFloat(pos);
                     pos += sizeFloat;
-                    'push_num $num';
+                    [Instruction('push_num'), Arg(num)];
+                    // 'push_num $num';
                 case ByteCodeOpValue.ConstantString:
                     var index = program.get(pos);
                     pos += 4;
-                    'constant_str $index';
-                case ByteCodeOpValue.Print: 'print';
-                case ByteCodeOpValue.Pop: 'pop ${program.get(pos++)}';
-                case ByteCodeOpValue.GetLocal: 'get_local ${program.get(pos++)}';
+                    [Instruction('constant_str'), Arg(index), Hint('(${bytecode.strings[index]})')];
+                    // 'constant_str $index ("${bytecode.strings[index]}")';
+                case ByteCodeOpValue.Print: [Instruction('print')];
+                case ByteCodeOpValue.Pop: [Instruction('pop ${program.get(pos++)}')];
+                case ByteCodeOpValue.GetLocal: [Instruction('get_local ${program.get(pos++)}')];
                 case ByteCodeOpValue.JumpIfFalse:
                     final offset = program.getInt32(pos);
                     pos += 4;
                     final absolute = pos + offset;
-                    'jump_if_false $offset (=> $absolute)';
-                case _: '[Unknown bytecode: "$code"]';
+                    [Instruction('jump_if_false'), Arg(offset), Hint('($ipPos => $absolute)')];
+                case _: [Error('[Unknown bytecode: "$code"]')];
             }
+
+            var disassembly = [ for (part in parts) color(part) ].join('\t');
             output += '$ipPos\t${pos - ipPos}B\t$disassembly\n';
         }
         return output;
