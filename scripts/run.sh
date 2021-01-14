432ea78d34e7fef869f54aeefe092b3aa93fbfe0
Anders Nissen <andnis@gmail.com>
2021-01-14 21:24:23 +0100

    Update jvm output paths

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
-# haxe build.hxml && java -jar bin/java/cosy.jar $@ 
-# haxe build.hxml && cp bin/js/cosy.js docs/playground/cosy.js && java -jar bin/java/cosy.jar $@ 
+# haxe build.hxml && java -jar bin/jvm/Cosy.jar $@ 
+# haxe build.hxml && cp bin/js/cosy.js docs/playground/cosy.js && java -jar bin/jvm/Cosy.jar $@ 
 
 # haxe -cp src -D no-inline --run cosy.Cosy $@
 haxe -cp src --run cosy.Cosy $@
