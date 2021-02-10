c05dbee53b46e0879f38bd2ed0ecbd073cd6ba58
Anders Nissen <andnis@gmail.com>
2021-02-10 21:56:02 +0100

    Update playground

--- scripts/update_playground.sh
+++ scripts/update_playground.sh
@@ -9,4 +9,4 @@ if [ $retVal -ne 0 ]; then
     exit $retVal
 fi
 cp bin/js/cosy.js docs/playground/cosy.js
-cp test/examples/cosy-basics.cosy docs/playground/examples/ex-cosy-basics.cosy
+cp test/examples/misc/cosy-basics.cosy docs/playground/examples/ex-cosy-basics.cosy
