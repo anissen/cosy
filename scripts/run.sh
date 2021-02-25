ee401f1e7d1438f98e2ef691a4e41a9e821b9aae
Anders Nissen <andnis@gmail.com>
2021-02-25 22:24:42 +0100

    Update build scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
-# haxe build.hxml && java -jar bin/jvm/Cosy.jar $@ 
-# haxe build.hxml && cp bin/js/cosy.js docs/playground/cosy.js && java -jar bin/jvm/Cosy.jar $@ 
+# haxe jvm.hxml && java -jar bin/jvm/cosy.jar $@ 
+# haxe jvm.hxml && cp bin/js/cosy.js docs/playground/cosy.js && java -jar bin/jvm/cosy.jar $@ 
 
 # haxe -cp src -D no-inline --run cosy.Cosy $@
 haxe -cp src --run cosy.Cosy $@
