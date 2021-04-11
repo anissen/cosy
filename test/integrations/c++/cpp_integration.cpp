845b97d12679fdd59d15ae13c9e8a8470fcdc613
Anders Nissen <andnis@gmail.com>
2021-04-11 20:44:52 +0200

    Mention some current C++ embedding limitations

--- test/integrations/c++/cpp_integration.cpp
+++ test/integrations/c++/cpp_integration.cpp
@@ -4,6 +4,10 @@
 // extern "C" void hxcpp_set_top_of_stack();
 extern "C" const char *hxRunLibrary();
 
+static void func() {
+	printf("func()\n");
+}
+
 int main() {
     // hxcpp_set_top_of_stack();
     const char *err = hxRunLibrary();
@@ -14,6 +18,17 @@ int main() {
 	}
 
     cosy::Cosy_obj::run("print 'hello from c++'");
-    cosy::Cosy_obj::runFile("test/examples/misc/mandelbrot.cosy");
+    
+	cosy::Cosy_obj::runFile("test/examples/misc/mandelbrot.cosy");
+
+	// Foreign variable works somewhat (int and bool works, string does not)
+    // cosy::Cosy_obj::setVariable("x", 42);
+    // cosy::Cosy_obj::setVariable("x", "asdf".c_str());
+    // cosy::Cosy_obj::run("foreign var x\nprint 'foreign variable: {x}'");
+	
+	// Foreign function callbacks do not work:
+    // cosy::Cosy_obj::setFunction("f", &func);
+    // cosy::Cosy_obj::run("foreign fn f()\nprint 'calling foreign func:'\nf()\nprint 'done'");
+	
 	return 0;
 }
