8e4930bb008eed4ff6d0718434779d09260c4086
Anders Nissen <andnis@gmail.com>
2023-04-13 21:05:48 +0200

    Misc

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,10 +2,10 @@
 # haxe scripts/jvm.hxml && java -jar bin/jvm/cosy.jar $@ 
 
 # Target: Node (JavaScript)
-haxe scripts/node.hxml && node bin/node/cosy.js $@
+# haxe scripts/node.hxml && node bin/node/cosy.js $@
 
 # Target: C++
 # haxe scripts/cpp.hxml && ./bin/cpp/cosy $@
 
 # Target: Haxe Eval
-# haxe scripts/eval_debug.hxml $@
+haxe scripts/eval_debug.hxml $@
